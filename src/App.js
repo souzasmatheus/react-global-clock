3c2d92cc892ac7dacc7a9525fc0eae75a9c23b60
Matheus Souza <matheu.sk.9@hotmail.com>
2018-11-26 22:38:43 -0200

    Build Buttton component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import Clock from './presentation/clock'
+import Button from './presentation/button'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Clock />
+        <Button styles="success">SEND</Button>
       </div>
     );
   }
