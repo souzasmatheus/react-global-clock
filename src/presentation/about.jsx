10e56601aa994fa02acfdd46a68ca83b2ad08e2c
Matheus Souza <matheu.sk.9@hotmail.com>
2018-11-27 16:51:15 -0200

    Create skeleton of About component

---
+++ src/presentation/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = () => (
+    <div className="about">
+        <h1>About</h1>
+        <p>Project bla bla bla</p>
+    </div>
+)
+
+export default About
--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import {Switch, Route, HashRouter} from 'react-router-dom'
 
 import Clock from '../presentation/clock'
+import About from '../presentation/about'
 
 const Routes = () => (
     <HashRouter>
         <Switch>
             <Route exact path="/" component={Clock}/>
+            <Route path="/about" component={About} />
         </ Switch>
     </HashRouter>
 )
