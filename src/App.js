21cfc65a2feba53cca92dd448fa3a2251fe19339
Matheus Souza <matheu.sk.9@hotmail.com>
2018-11-26 22:10:11 -0200

    Build Clock component skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Clock from './presentation/clock'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world</h1>  
+        <Clock />
       </div>
     );
   }
