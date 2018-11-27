670f1f555c8dd8c509fcd22ff58da1f5382dea96
Matheus Souza <matheu.sk.9@hotmail.com>
2018-11-27 17:56:50 -0200

    Update Menu with link components

--- src/presentation/menu.jsx
+++ src/presentation/menu.jsx
@@ -5,6 +5,16 @@ const Menu = ({onSearch=f=>f}) => (
     <nav className="navbar navbar-light bg-light">
         <a href="/home" className="navbar-brand">Global Clock</a>
 
+        <ul className="navbar-nav flex-row">
+            <li className="nav-item mr-2">
+                <a className="nav-link" href="/">Home</a>
+            </li>
+            <li className="nav-item mr-2">
+                <a className="nav-link" href="#/about">About</a>
+            </li>
+        </ul>
+        
+
         <div className="ml-auto">
             <SearchForm />
         </div>
