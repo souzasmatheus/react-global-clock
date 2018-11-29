2da00a1bb59a844d31f72d9c0e32ad7dfaec5b05
Matheus Souza <matheu.sk.9@hotmail.com>
2018-11-28 22:49:43 -0200

    Simplify SearchForm compoment

--- src/templates/search.jsx
+++ src/templates/search.jsx
@@ -1,31 +1,23 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Button from './button'
 import PropTypes from 'prop-types'
 
-class SearchForm extends Component {
-    constructor(props) {
-        super(props)
-
-        this.submit = this.submit.bind(this)
-    }
-
-    submit(e) {
+const SearchForm = ({onSearch=f=>f}) => {
+    let _cityName
+    const submit = e => {
         e.preventDefault()
-        const {_cityName} = this.refs
-        this.props.onSearch(_cityName.value)
+        onSearch(_cityName.value)
     }
 
-    render() {
-        return (
-            <form className="form-inline search-form" onSubmit={this.submit}>
-                <div className="form-group mr-2">
-                    <input type="text" className="form-control" 
-                    ref="_cityName" placeholder="Type a city name..."/>
-                </div>
-                <Button styles="success" type="submit">SEARCH</Button>
-            </form>
-        )
-    }
+    return (
+        <form className="form-inline search-form" onSubmit={submit}>
+            <div className="form-group mr-2">
+                <input type="text" className="form-control" 
+                ref={input => _cityName = input} placeholder="Type a city name..."/>
+            </div>
+            <Button styles="success" type="submit">SEARCH</Button>
+        </form>
+    )
 }
 
 SearchForm.propTypes = {
