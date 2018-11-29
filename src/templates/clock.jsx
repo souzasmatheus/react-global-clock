3798363b83bd23af3dcfb606f46c141b91f24059
Matheus Souza <matheu.sk.9@hotmail.com>
2018-11-29 01:30:32 -0200

    Add first digit verification on Clock component

--- src/templates/clock.jsx
+++ src/templates/clock.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 
 const Clock = ({hour='00', minute='00', ampm=' '}) => (
     <div>
-        <span>{hour}:</span>
-        <span>{minute}</span>
+        <span>{(hour > 9 || hour === '00') ? hour : '0' + hour}:</span>
+        <span>{(minute > 9 || minute === '00') ? minute : '0' + minute}</span>
         <span> </span>
         <span>{ampm}</span>
     </div>
