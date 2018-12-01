a39dea9cc7dd44a6585dabe7f930e21015345642
Matheus Souza <matheu.sk.9@hotmail.com>
2018-12-01 13:51:50 -0200

    Redimension clock on larger screens

--- src/templates/clock.jsx
+++ src/templates/clock.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Clock = ({hour=0, minute=0, ampm=' '}) => (
 
-    <div className="col-6">
+    <div className="col-6 col-md-4">
         <div className="clockBorder p-3">
             <div className="clockDisplay">
                 <span>{(hour > 9) ? hour : '0' + hour}:</span>
