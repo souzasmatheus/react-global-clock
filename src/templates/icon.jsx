001c58a78b3d2fcdc68634eff3432bb671af2e93
Matheus Souza <matheu.sk.9@hotmail.com>
2018-12-05 23:26:59 -0200

    Improve Icon component's security

--- src/templates/icon.jsx
+++ src/templates/icon.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Icon = ({site, children}) => (
     <div className="icon d-inline-block mx-1 mt-2 mb-5 pt-3">
-        <a href={site} target="_blank">
+        <a href={site} rel="noopener noreferrer" target="_blank">
             <i className={children}></i>
         </a>
     </div>
