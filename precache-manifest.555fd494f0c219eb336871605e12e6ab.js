4c19e2cc03aad358fb6f12c82c558f3aea4cccb5
Matheus Souza <matheu.sk.9@hotmail.com>
2018-12-06 18:56:36 -0200

    Updates

---
+++ precache-manifest.555fd494f0c219eb336871605e12e6ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/react-global-clock/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "efbea3599410ee263e1f",
+    "url": "/react-global-clock/static/js/runtime~main.efbea359.js"
+  },
+  {
+    "revision": "294b212036a8c4abeb32",
+    "url": "/react-global-clock/static/js/main.294b2120.chunk.js"
+  },
+  {
+    "revision": "eb3c132fcbcda75590a6",
+    "url": "/react-global-clock/static/js/1.eb3c132f.chunk.js"
+  },
+  {
+    "revision": "294b212036a8c4abeb32",
+    "url": "/react-global-clock/static/css/main.d3273825.chunk.css"
+  },
+  {
+    "revision": "eb3c132fcbcda75590a6",
+    "url": "/react-global-clock/static/css/1.6e5f1c6c.chunk.css"
+  },
+  {
+    "revision": "3d515acd6c110fb8df6630fb2a1affa0",
+    "url": "/react-global-clock/index.html"
+  }
+];
