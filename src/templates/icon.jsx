bb0380b56a76db14debf4522fd2a517be9e3f647
Matheus Souza <matheu.sk.9@hotmail.com>
2018-12-05 23:23:52 -0200

    Complete About component

---
+++ src/templates/icon.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Icon = ({site, children}) => (
+    <div className="icon d-inline-block mx-1 mt-2 mb-5 pt-3">
+        <a href={site} target="_blank">
+            <i className={children}></i>
+        </a>
+    </div>
+)
+
+Icon.propTypes = {
+    site: PropTypes.string,
+    children: PropTypes.string
+}
+
+export default Icon
