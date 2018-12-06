60125635e6e4198f1a7f604f00c80a724695c262
Matheus Souza <matheu.sk.9@hotmail.com>
2018-12-05 22:17:30 -0200

    Partially redesign About component

--- src/templates/about.jsx
+++ src/templates/about.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const About = () => (
-    <div className="about">
-        <h1>About</h1>
-        <p>Project bla bla bla</p>
+    <div className="about col-10 col-md-6">
+        <h1 className="display-3">Hello, stranger!</h1>
+        <p>Thank you for exploring the React Global Clock! </p>
+        <p>I am Matheus Souza and this is a portfolio project I have developed using React. </p>
+        <p>Check out my other projects:</p>
     </div>
 )
 
